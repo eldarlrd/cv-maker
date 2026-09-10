6a443fdd64069b59ca03026db16abd35e379af86
eldarlrd <eldarlrd@pm.me>
2026-09-10 13:21:33 +0400

    chore(errors): more semantic message for confirmation error

--- src/config/errors.ts
+++ src/config/errors.ts
@@ -1,4 +1,4 @@
 const ERROR_SERVICE_WORKER = 'Service Worker Register failed.';
-const ERROR_CONFIRMATION_HOOK = 'useConfirmation not in ConfirmationProvider.';
+const ERROR_CONFIRMATION_HOOK = 'useConfirmation outside ConfirmationProvider.';
 
 export { ERROR_CONFIRMATION_HOOK, ERROR_SERVICE_WORKER };
